0a3e28e1aa5b7cf2ed3a1139b15cd3be8655f01d
Scott <scott@digression.org>
2014-05-15 20:53:57 -0700

    Added bootstrap and tweaked a bunch of initial html.

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, g
 from .views import frontend
 
-flask_app = Flask('sleepy', template_folder='app/web/templates')
+flask_app = Flask('sleepy', template_folder='app/web/templates', static_folder='app/web/static')
 flask_app.debug = True
 
 flask_app.register_blueprint(frontend)
